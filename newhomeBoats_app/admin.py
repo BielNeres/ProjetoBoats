52ea4e02407240d26288974359a30b3aab09c539
BielNeres <biel.neres11@hotmail.com>
2019-12-31 11:16:00 -0300

    Adicionando campos de cidade, estado e Ibgecodes ao acesso do administrador

--- newhomeBoats_app/admin.py
+++ newhomeBoats_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import City, State, IBGECityCodes
 # Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    search_fields = ['city']
+    list_display = ['city', 'state']
+    pass
+admin.site.register(City, CityAdmin)
+
+admin.site.register(State)
+
+
+class IBGECityCodesAdmin(admin.ModelAdmin):
+    search_fields = ['ibge_id', 'name']
+    list_display = ['ibge_id', 'name']
+    pass
+admin.site.register(IBGECityCodes, IBGECityCodesAdmin)
